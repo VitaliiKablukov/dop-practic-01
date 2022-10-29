3592699f1e12748aa44593facabbd201818be68e
HelenaGingembre <e.izotovagingembre@outlook.com>
2022-10-29 12:16:01 +0300

    start search form

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,10 +1,41 @@
 import { Component } from 'react';
-
+import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
+import { nanoid } from 'nanoid';
 
-export class SearchForm extends Component {
-  render() {
-    return <h2>SearchForm</h2>;
-  }
-}
+export const SearchForm = ({ getTodo }) => {
+  const [query, setQuery] = useState('');
+
+  const handleInput = event => {
+    setQuery(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    const todo = {
+      id: nanoid(),
+      text: query,
+    };
+
+    getTodo(todo);
+    setQuery('');
+  };
+
+  return (
+    <SearchFormStyled onSubmit={handleSubmit}>
+      <FormBtn type="submit">
+        <FiSearch size="16px" />
+      </FormBtn>
+      <InputSearch
+        onChange={handleInput}
+        placeholder="What do you want to write?"
+        name="search"
+        required
+        autoFocus
+        value={query}
+      />
+    </SearchFormStyled>
+  );
+};
