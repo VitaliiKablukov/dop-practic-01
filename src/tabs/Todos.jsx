2e0ea23a5396cfdde2d72ebeec9a3e4268dbfd8f
Vitaliy Ivanov <dotaivanov@gmail.com>
2022-10-29 12:43:23 +0300

    feat: add grid

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,9 +1,40 @@
-import { Component } from 'react';
 import { nanoid } from 'nanoid';
 import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
+import { useEffect, useState } from 'react';
 
-export class Todos extends Component {
-  render() {
-    return <SearchForm getTodo={todo => console.log(todo)} />;
+const parseDataFromLS = (key, initialValue = []) => {
+  try {
+    return JSON.parse(localStorage.getItem(key)) ?? initialValue;
+  } catch (error) {
+    return initialValue;
   }
-}
+};
+export const Todos = () => {
+  const [todos, setTodos] = useState(() => parseDataFromLS('todos'));
+  const addTodo = todo => {
+    setTodos(p => [{ ...todo, id: nanoid(6) }, ...p]);
+  };
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  return (
+    <>
+      <SearchForm getTodo={addTodo} />
+      {todos.length > 0 ? (
+        <Grid>
+          {todos.map(({ id, text }) => {
+            return (
+              <GridItem key={id}>
+                <Todo text={text} />
+              </GridItem>
+            );
+          })}
+        </Grid>
+      ) : (
+        <p>There is no todo yet...</p>
+      )}
+    </>
+  );
+};
